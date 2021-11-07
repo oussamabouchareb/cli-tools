6baf5d5aee9bfcc28b4c2dcd5018637571d802c8
oussama.bouchareb <oussamabouchareb006@gmail.com>
2021-11-07 15:37:06 +0100

    made typeorm optional and fixed some comments

--- src/modules/createModule.ts
+++ src/modules/createModule.ts
@@ -9,40 +9,58 @@ import {
 } from "../templates";
 import { capitalizeFirstLetter } from "../utils";
 
-export function createModule(moduleName: string) {
+export function createModule(moduleName: string, typeorm: boolean) {
   const pathToModule = join("src", "modules", moduleName);
   const pathToORM = join("src", "orm");
   const pathToEntity = join(pathToORM, "entity");
 
-  createDirectory(pathToORM);
-  createDirectory(pathToEntity);
-  // root module folder
-  createDirectory(pathToModule);
+  if (typeorm === true) {
+    // create typeorm folder
+    createDirectory(pathToORM);
 
-  // graphql
-  createDirectory(join(pathToModule, "graphql"));
-  // type.graphql
-  createFile(
-    join(pathToModule, "graphql", "type.graphql"),
-    typeTemplate(moduleName)
-  );
-  // operations.graphql
-  createFile(
-    join(pathToModule, "graphql", "operations.graphql"),
-    operationsTemplate(moduleName)
-  );
-  // mutations.ts
-  createFile(join(pathToModule, "mutations.ts"), mutationsTemplate(moduleName));
-  // schema.ts
-  createFile(join(pathToModule, "schema.ts"), schemaTemplate(moduleName));
-  // index.ts
-  createFile(join(pathToModule, "index.ts"), 'export * from "./schema"');
-  // validations.ts
-  createFile(join(pathToModule, "validations.ts"), "");
-  // queries.ts
-  createFile(join(pathToModule, "queries.ts"), "");
-  createFile(
-    join(pathToEntity, `${capitalizeFirstLetter(moduleName)}.ts`),
-    entityTemplate(moduleName)
-  );
+    // create entity folder for typeorm
+    createDirectory(pathToEntity);
+
+    // Create Entity File for current module
+    createFile(
+      join(pathToEntity, `${capitalizeFirstLetter(moduleName)}.ts`),
+      entityTemplate(moduleName)
+    );
+  } else {
+    // root module folder
+    createDirectory(pathToModule);
+
+    // graphql
+    createDirectory(join(pathToModule, "graphql"));
+
+    // type.graphql
+    createFile(
+      join(pathToModule, "graphql", "type.graphql"),
+      typeTemplate(moduleName)
+    );
+
+    // operations.graphql
+    createFile(
+      join(pathToModule, "graphql", "operations.graphql"),
+      operationsTemplate(moduleName)
+    );
+
+    // mutations.ts
+    createFile(
+      join(pathToModule, "mutations.ts"),
+      mutationsTemplate(moduleName)
+    );
+
+    // schema.ts
+    createFile(join(pathToModule, "schema.ts"), schemaTemplate(moduleName));
+
+    // index.ts
+    createFile(join(pathToModule, "index.ts"), 'export * from "./schema"');
+
+    // validations.ts
+    createFile(join(pathToModule, "validations.ts"), "");
+
+    // queries.ts
+    createFile(join(pathToModule, "queries.ts"), "");
+  }
 }
