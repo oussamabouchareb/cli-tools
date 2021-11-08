bc7ce2ce6363df76a6747a92db039b704aeb58f8
oussama.bouchareb <oussamabouchareb006@gmail.com>
2021-11-08 17:23:10 +0100

    made some changes for buikding the app

---
+++ rollup.config.ts
@@ -0,0 +1,39 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-typescript2";
+import babel from "@rollup/plugin-babel";
+import shebang from "rollup-plugin-preserve-shebang";
+import rjson from "@rollup/plugin-json";
+
+import packageJson from "./package.json";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/env", "@babel/preset-typescript"],
+      plugins: ["@babel/plugin-proposal-class-properties"],
+      babelHelpers: "bundled",
+    }),
+    typescript(),
+    shebang(),
+    rjson(),
+  ],
+};
